c78831b220ae265f3fdf81946ca9b967a9c9add9
GJ98 <gangjeong22@naver.com>
2021-03-06 14:07:29 +0000

    init implementation

--- GJchatbot/model/embed/fasttext.py
+++ GJchatbot/model/embed/fasttext.py
@@ -3,17 +3,17 @@ from gensim.models import FastText
 
 class FastText(FastText):
 
-	def __init__(self):
-		"""Gensim FastText 모델 클래스"""
+    def __init__(self):
+        """Gensim FastText 모델 클래스"""
 
-		vector_size = 128
-		window_size = 2
-		workers = 8
-		min_count = 2
-		iter = 2000
+        vector_size = 128
+        window_size = 2
+        workers = 8
+        min_count = 2
+        iter = 2000
 
-		super().__init__(size=vector_size,
-						 window=window_size,
-						 workers=workers,
-						 min_count=min_count,
-						 iter=iter)
+        super().__init__(size=vector_size,
+                         window=window_size,
+                         workers=workers,
+                         min_count=min_count,
+                         iter=iter)
